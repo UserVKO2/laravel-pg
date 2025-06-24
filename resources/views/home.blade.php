cee9b94be367cd7a2ce9cde7ecc784f070c8b8e1
UserVKO2 <medet.uk@gmail.com>
2025-06-25 01:04:34 +0600

    Добавил главную страницу

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Home page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dolorum ullam necessitatibus saepe magnam, numquam itaque? Nulla natus, ipsa, est commodi itaque omnis quia labore at incidunt dolor ipsam blanditiis?</p>
+</body>
+</html>
